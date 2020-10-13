f00f96eb42d259fd7dc7142e06271a7bb7dad4fd
tobi-oye <tobiloba5000@gmail.com>
2020-10-13 12:43:42 +0100

    front-page working

--- src/App.js
+++ src/App.js
@@ -12,27 +12,43 @@ class BooksApp extends React.Component {
      */
     showSearchPage: false,
  
-    bookShelves: [{shelveName:"Currently Reading",books:[]}, {shelveName:"Want to Read",books:[]}, {shelveName:"Read",books:[]}],
-    options: ["Currently Reading", "Want to Read", "Read", "None"],
+    bookShelves: {currentlyReading:{shelveName:"Currently Reading",books:[]}, wantToRead:{shelveName:"Want to Read",books:[]}}, read:{shelveName:"Read",books:[]},
+    options: {currentlyReading:{label:"Currently Reading",id:"currentlyReading"}, wantToRead:{label:"Want to Read",id:"wantToRead"}, read:{label:"Read",id:"read"}, none:{label:"None",id:"none"}},
     allBooks:[],
-    currentlyReading:[],
-    wantToRead:[],
-    read:[]
   };
 
   /**
    * function to move book from one book shelve to the other
    * @param {*} event
+   * it will get element id and clicked value from the click event
+   * conditional statement for the various values 
+   *  if true add the new element id  to the array and filter the previous element based on id
+   * 
    */
-
-
-  moveBook = (e) => {
-    // let elem;
-  };
+  // moveBook = (singleElem,optionvalue,elem) => {
+  //   //  console.log(elem,e.target.value)
+  //   let newValue = options.target.value
+  //   if(newValue === "currentlyReading"){
+  //     BooksAPI.update(singleElem,newValue) 
+  //     elem.filter((element) => {
+  //       this.setState((previouState)=>{
+  //         previouState.
+  //       })
+  //     })
+  //     }
+  //   }
+    
+  // };
+  
 
   componentDidMount(){
     BooksAPI.getAll().then((res) => {
-      return this.setState({allBooks:res, bookShelves:[{shelveName:"Currently Reading",books: res.splice(0,2)}, {shelveName:"Want to Read",books:res.splice(2,2)}, {shelveName:"Read",books:res}]
+      return this.setState({allBooks:res, 
+        bookShelves:{
+          currentlyReading: {shelveName:"Currently Reading",books: res.splice(0,2)}, 
+        wantToRead:{shelveName:"Want to Read",books:res.splice(2,2)}, 
+        read:{shelveName:"Read",books:res}
+      }
     })
   })
 }
@@ -40,14 +56,14 @@ class BooksApp extends React.Component {
 
 
   render() {
-   const {showSearchPage,bookShelves,options,allBooks = []} = this.state
+   const {showSearchPage,bookShelves,options,allBooks} = this.state;
   //  console.log(this.state.wantToRead)
 
     return (
       <div className="app">
            
         {this.state.showSearchPage ? (
-          <div className="search-books">
+          <div className="search-books">h
             <div className="search-books-bar">
               <button
                 className="close-search"
@@ -79,12 +95,12 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                {this.state.bookShelves.map((elem) => {
+                {Object.values(bookShelves).map((book) => {
                                  return  <div className="bookshelf">
-                                  <h2 className="bookshelf-title">{elem.shelveName}</h2>
+                                  <h2 className="bookshelf-title">{book.shelveName}</h2>
                                   <div className="bookshelf-books">
                                     <ol className="books-grid currentlyReading">
-                                    {elem.books.map((elem) => {
+                                    {book.books.map((img) => {
                                       return  <li>
                                       <div className="book">
                                         <div className="book-top">
@@ -93,29 +109,29 @@ class BooksApp extends React.Component {
                                             style={{
                                               width: 128,
                                               height: 193,
-                                              backgroundImage: `url(${elem.imageLinks.smallThumbnail})`,
+                                              backgroundImage: `url(${img.imageLinks.smallThumbnail})`,
                                             }}
                                           />
                 
                                           <div className="book-shelf-changer">
                                             <select
                                               onClick={(e) => {
-                                                return this.moveBook(e);
+                                                return this.moveBook(book,e);
                                               }}
                                             >
                                               <option value="move" disabled>
                                                 Move to...
                                               </option>
-                                              {this.state.options.map((elem) => {
-                                                return <option> {elem} </option>;
+                                              {Object.values(options).map((elem) => {
+                                                return <option value ={elem.id} > {elem.label} </option>;
                                               })}
                                             </select>
                                           </div>
                                         </div>
                                         <div className="book-title">
-                                          {elem.title}
+                                          {img.title}
                                         </div>
-                                            <div className="book-authors">{elem.authors[0]}</div>
+                                            <div className="book-authors">{img.authors[0]}</div>
                                       </div>
                                     </li>
                                     })}
@@ -135,6 +151,6 @@ class BooksApp extends React.Component {
       </div>
     );
   }
-}
+};
 
 export default BooksApp;
